7078957d66caed3bf601dd26f523dcd2acfd5c45
Alex Gordon <alexg1991@outlook.com>
2024-12-27 14:43:09 +0000

    Create home app and add to settings as a installed app, include templates in home directory, set up index template that has extended the base template, define views and config urls

--- crop_circle/settings.py
+++ crop_circle/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'django_extensions',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -60,12 +61,15 @@ ROOT_URLCONF = 'crop_circle.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',  # required by allauth
+                'django.template.context_processors.request',  # required allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
