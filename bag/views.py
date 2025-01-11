18bfe8065e56b4cb2bfdad471cec3f5b483bf508
Alex Gordon <alexg1991@outlook.com>
2025-01-11 14:32:04 +0000

    Add toast messages in view, also include comments to help explain line by line what is happening in the code, always helps!

--- bag/views.py
+++ bag/views.py
@@ -17,10 +17,10 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ This function handles adding a product to the shopping bag."""
 
-    # It retrieves the product quantity and redirect URL from the POST request.
+    # Retrieve the product quantity and redirect URL from the POST request.
     # If the product already exists in the bag (stored in the session),
-    # it increments the quantity; otherwise,
-    # it adds the product with the specified quantity.
+    # increment the quantity; otherwise,
+    # add the product with the specified quantity.
     # The updated bag is saved back into the session,
     # and the user is redirected to the specified URL.
 
@@ -31,6 +31,9 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request,
+                         (f'Updated {product.name} '
+                          f'quantity to {bag[item_id]}'))
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.name} to your bag')
@@ -41,6 +44,19 @@ def add_to_bag(request, item_id):
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
+
+    # Retrieve the product using the provided item_id.
+    # If not found, a 404 error is raised.
+    # Get the new quantity from the POST request and convert it to an integer.
+    # Retrieve bag from the session, if not found initialise as empty dict.
+    # Check if the new quantity is greater than 0.
+    # If so, update the quantity of the product in the bag.
+    # Set the new quantity for the product in the bag,
+    # Success message sent to the user.
+    # If the quantity is 0 or less, remove the product from the bag,
+    # Success message sent to the user.
+    # The updated bag is saved back into the session,
+    # and the user is redirected to the specified URL.
 
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
@@ -63,6 +79,16 @@ def adjust_bag(request, item_id):
 
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
+
+    # Retrieve the product using the provided item_id.
+    # If not found, a 404 error is raised.
+    # Retrieve bag from the session, if not found initialize as empty dict.
+    # Remove the item from the bag using its item_id.
+    # Success message sent to the user.
+    # Update the session with the modified bag (without the removed item).
+    # Return HTTP response (status 200) to indicate operation was successful.
+    # If any error occurs, display an error message.
+    # Return HTTP response (status 500) to indicate an internal server error.
 
     try:
         product = get_object_or_404(Product, pk=item_id)
