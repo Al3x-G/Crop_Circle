ab3242692a64a819f1c792e0753a3614a968491a
Alex Gordon <alexg1991@outlook.com>
2025-01-12 13:05:44 +0000

    Create a payment intent with stripe

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,9 @@ import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "Looks like there is nothing in your bag.")
@@ -21,13 +24,22 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. Did you forget to set it in your environment?')  # noqa: E501 (Fix flake 8 line too long)
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_TYooMQauvdEDq54pk_test_51QgGfSGPAr31TKtuWLsYtlanGft2gHeDUNGHnr01WX1VpjFPqxMgsB9Ni62uODesAToBdFSjl2A5F78YqqsnA5t900Ly9FoR4KNiTphI7jx',  # noqa: E501 (Fix flake 8 line too long)
-        'client_secret': 'CLIENT_SECRET',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
