4bff898f41aadbe0c2f0fdca61bec9d256d06051
Alex Gordon <alexg1991@outlook.com>
2025-01-16 22:22:41 +0000

    Flake 8 fixes, extend login required to product views

--- products/models.py
+++ products/models.py
@@ -19,12 +19,12 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)  # noqa: E501 (Fix flake 8 line too long)
     sku = models.CharField(max_length=250, null=True, blank=True)
     name = models.CharField(max_length=250)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=1, null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=1, null=True, blank=True)  # noqa: E501 (Fix flake 8 line too long)
     image_url = models.URLField(max_length=1025, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserProfileForm
 from .models import UserProfile
 from checkout.models import Order
 
 
+@login_required
 def profile(request):
     """
     Display the user's profile and allow updates to profile information.
