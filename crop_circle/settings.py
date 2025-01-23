eab3530899f19e8b8516d6e344261226c468b6f7
Alex Gordon <alexg1991@outlook.com>
2025-01-23 20:20:52 +0000

    Debug config variables

--- crop_circle/settings.py
+++ crop_circle/settings.py
@@ -10,26 +10,28 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from dotenv import load_dotenv
 import os
 import dj_database_url
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.getenv("SECRET_KEY", '')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'crop-circle-app-ecab80dfedd0.herokuapp.com',
-    '8000-al3xg-cropcircle-uty04ds9k0t.ws.codeinstitute-ide.net'
+    '8000-al3xg-cropcircle-uty04ds9k0t.ws.codeinstitute-ide.net',
+    'localhost'
     ]
 
 
